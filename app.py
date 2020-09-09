23908e192177fdedae3c36c5e7c5d5be4c3d457d
waynechiu <hjk25323100@gmail.com>
2020-09-09 14:08:04 +0800

    login頁面（豬豬）&正要導入SQL

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask,render_template
 
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+import router
+import os
+
+static_dir='app/static'
+
+template_dir = 'app/templates'
+
+app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 
 @app.route('/')
 @app.route('/<name>')
@@ -9,5 +21,10 @@ def index(name=None):
 
 
 @app.route('/login')
-def login_page(name=None):
+def login_page(request=None):
     return render_template("login.html")
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
